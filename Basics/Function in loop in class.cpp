929a7b05d65550bb8c616ffa9d4f641fd957dea9
Avi Kapoor <85234265+avikapoor24@users.noreply.github.com>
2022-12-14 21:22:22 +0530

    Update Function in loop in class.cpp

--- Basics/Function in loop in class.cpp
+++ Basics/Function in loop in class.cpp
@@ -1,17 +1,17 @@
 
-
 #include<iostream>
 using namespace std;
 class demo
 { public:
     int c;
-    fun(int a, int b)
+    int fun(int a, int b)
     {
          c=a+b;
         cout<<c;
+        return c;
     }
 };
-main()
+int main()
 {
      int a,b,i;
          demo st[5];
